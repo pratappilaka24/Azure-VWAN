5f428b1102317c2fcc89deae6716950e00d5509e
Pratap Pilaka <pratappilaka@1yearsub.onmicrosoft.com>
2023-06-05 08:21:59 +0000

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,8 @@
 terraform {
-  backend "local" {
-   
+  backend "azurerm" {
+    resource_group_name  = "rg_storageaccount-aue-001"
+    storage_account_name = "saterraformstateaue001"
+    container_name       = "tfstate"
+    key                  = "vwantfstate.tfstate"
   }
 }
