94a0399eec09a285c89eb28da0a2ce8165aa7ff9
Pratap Pilaka <pratappilaka@1yearsub.onmicrosoft.com>
2023-06-05 10:48:51 +0000

    Updated resourcegroups.tf

--- resourcegroups.tf
+++ resourcegroups.tf
@@ -1,6 +1,6 @@
 # Resource Groups Virtual-WAN - Region 1
 resource "azurerm_resource_group" "region1-rg1" {
-  name     = "rg-${var.region1}-Virtual-WAN-Demo"
+  name     = "rg-${var.region1}-VWAN-Demo"
   location = var.region1
   tags = {
     Environment = var.environment_tag
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "region1-rg1" {
 }
 # Resource Groups Virtual-WAN - Region 2
 resource "azurerm_resource_group" "region2-rg1" {
-  name     = "rg-${var.region2}-Virtual-WAN-Demo"
+  name     = "rg-${var.region2}-VWAN-Demo"
   location = var.region2
   tags = {
     Environment = var.environment_tag
