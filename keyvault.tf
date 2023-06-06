ef304c3c9eb4c226e1f78639d6f1c220c734d586
Pratap Pilaka <pratappilaka@1yearsub.onmicrosoft.com>
2023-06-06 08:51:56 +0000

    Updated keyvault.tf

--- keyvault.tf
+++ keyvault.tf
@@ -49,5 +49,5 @@ resource "azurerm_key_vault_secret" "vmpassword" {
   name         = "vmpassword"
   value        = random_password.vmpassword.result
   key_vault_id = azurerm_key_vault.kv1.id
-  depends_on   = [azurerm_key_vault.kv1]
+  depends_on   = [azurerm_key_vault_access_policy.default_policy]
 }
