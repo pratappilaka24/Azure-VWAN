85c620a7fba3237b91b05b8f83d32c42def39480
Pratap Pilaka <pratappilaka@1yearsub.onmicrosoft.com>
2023-06-11 01:34:21 +0000

    Updated virtualmachines.tf

--- virtualmachines.tf
+++ virtualmachines.tf
@@ -1,4 +1,25 @@
 # Virtual Machines
+#creating pips
+resource "azurerm_public_ip" "region1-pip" {
+  name                = "${var.region1}-pip"
+  location            = var.region1
+  resource_group_name = azurerm_resource_group.region1-rg1.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags = {
+    Environment = var.environment_tag
+  }
+}
+resource "azurerm_public_ip" "region2-pip" {
+  name                = "${var.region2}-pip"
+  location            = var.region2
+  resource_group_name = azurerm_resource_group.region2-rg1.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags = {
+    Environment = var.environment_tag
+  }
+}
 #Create NICs and associate the Public IPs
 resource "azurerm_network_interface" "region1-vm01-nic" {
   name                = "${var.region1}-vm01-nic"
@@ -10,6 +31,7 @@ resource "azurerm_network_interface" "region1-vm01-nic" {
     name                          = "${var.region1}-vm01-ipconfig"
     subnet_id                     = azurerm_subnet.region1-vnet1-snet1.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          =  azurerm_public_ip.region1-pip.id
   }
 
   tags = {
@@ -26,6 +48,7 @@ resource "azurerm_network_interface" "region2-vm01-nic" {
     name                          = "${var.region2}-vm01-ipconfig"
     subnet_id                     = azurerm_subnet.region2-vnet1-snet1.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          =  azurerm_public_ip.region2-pip.id
   }
 
   tags = {
@@ -82,10 +105,10 @@ resource "azurerm_windows_virtual_machine" "region2-vm01" {
     storage_account_type = "StandardSSD_LRS"
   }
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
+ source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 }
